fed594e902168e2f95f787192c06c88770afcd5a
anish.abraham <idofanish@gmail.com>
2024-02-08 12:26:54 +0530

    Amended the Test Status Message

--- tests.py
+++ tests.py
@@ -5,15 +5,15 @@ from main import get_sum
 class MyTestCase(unittest.TestCase):
     def test1_get_sum(self):
         sum = get_sum(12,4)
-        self.assertEqual(sum , 16,"Pass")
+        self.assertEqual(sum , 16,"Test Pass")
 
     def test2_get_sum(self):
         sum = get_sum(12,4)
-        self.assertFalse(sum > 110,"Pass")
+        self.assertFalse(sum > 110,"Test Pass")
 
     def test3_get_sum(self):
         sum = get_sum(12,4)
-        self.assertTrue(sum < 210, "Pass")
+        self.assertTrue(sum < 210, "Test Pass")
 
 if __name__ == '__main__':
     unittest.main()
